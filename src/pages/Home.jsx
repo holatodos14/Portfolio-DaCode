8635ba3bb69196ae87b62dac7eb283b3659f7dfb
holatodos14 <131733759+holatodos14@users.noreply.github.com>
2025-08-28 02:44:06 -0500

    Portfolio Dacode

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import AboutSection from "../components/AboutSection";
+import ContactSection from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+import HeroSection from "../components/HeroSection";
+import Navbar from "../components/NavBar";
+import ProjectsSection from "../components/ProjectsSection";
+import SkillsSection from "../components/SkillsSection";
+import StarBackground from "../components/StarBackground";
+import ThemeToggle from "../components/ThemeToggle";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      
+      {/* Theme Toggle */}
+        <ThemeToggle />
+      {/* Background Effects */}
+        <StarBackground />
+      {/* NavBar */}
+        <Navbar/>
+      {/* Main Content */}
+        <main>
+        <HeroSection/>
+        <AboutSection/>
+        <SkillsSection/>
+        <ProjectsSection/>
+        <ContactSection/>
+        </main>
+
+      {/* Footer */}
+        <Footer/>
+    </div>
+  )
+}
